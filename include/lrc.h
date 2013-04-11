98a10bb717542834b06b59f075c7515f1e5a616e
Peter Eisenhower <peteeisen21@gmail.com>
2013-04-11 12:52:22 -0400

    Added LCR macro

--- include/lrc.h
+++ include/lrc.h
@@ -5,3 +5,10 @@
 #define LRC_INITIAL 0xFF
 
 uint8_t el_lrc8 (uint8_t initial, const uint8_t *data, size_t length);
+
+
+/**
+ * Add a single byte of data to the LRC
+ */
+#define EL_LRC8(initial, data)      (initial ^= data)
+
